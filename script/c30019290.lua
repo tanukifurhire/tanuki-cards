e7cc605953a7e7f55b663dda99383968db58b10e
Tanuki Fur Hire <xy4nid3@gmail.com>
2022-10-28 20:50:34 +0800

    Added Territory of the Sharks

---
+++ script/c30019290.lua
@@ -0,0 +1,38 @@
+--Territory of the Sharks
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop,1)
+end
+function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(ep,id)>1 then return end
+	--condition
+	return aux.CanActivateSkill(tp)
+	and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_WATER),tp,LOCATION_MZONE,0,1,nil)
+end
+function s.flipop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+	Duel.Hint(HINT_CARD,tp,id)
+	local atk=Duel.GetMatchingGroupCount(aux.FaceupFilter(Card.IsAttribute,ATTRIBUTE_WATER),tp,LOCATION_MZONE,0,nil)
+	if or atk==0 then return end
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
+		e1:SetReset(RESET_EVENT+PHASE_END)
+		e1:SetValue(4)
+		tc:RegisterEffect(e1)
+	end
+	Duel.RegisterFlagEffect(ep,id,0,0,0)
+	--flip during End phase
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.EPop)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.EPop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SKILL_FLIP,tp,id|(2<<32))
+end
