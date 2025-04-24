ab9b64ceaae76bd11511cd45c1edcc5759b3f341
Tai Xiao Xuan <xiaoxuan.tai@digipen.edu>
2025-04-24 20:00:20 +0800

    Update c92909400.lua

--- script/c92909400.lua
+++ script/c92909400.lua
@@ -106,10 +106,7 @@ end
 
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local rt=Duel.GetTargetCount(aux.TRUE,tp,0,LOCATION_ONFIELD,nil)
-	if rt <= 0 
-		return false 
-	end
-	
+
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
 	local cg=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE,0,1,rt,nil,lg)
 	local count = Duel.SendtoGrave(cg, REASON_COST)
